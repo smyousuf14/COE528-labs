6e28629829f81be314731b246d99b25df0b578b0
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-09-08 18:16:22 -0400

    Add files via upload

---
+++ lab1/FlightBooking/src/coe528/lab1/Passenger.java
@@ -0,0 +1,19 @@
+/*
+ * A typical passenger that has the common attribute that all passengers have.
+ */
+package coe528.lab1;
+
+/**
+ *
+ * @author Syed Yousuf
+ */
+public abstract class Passenger 
+{
+    // Instance Variables
+    private String name;
+    private int age;
+    
+    // Constructors 
+    
+    
+}
---
+++ lab1/FlightBooking/src/coe528/lab1/Ticket.java
@@ -0,0 +1,21 @@
+/*
+ * The Ticket class represents a flight ticket
+ */
+package coe528.lab1;
+
+/**
+ *
+ * @author Syed Yousuf
+ */
+public class Ticket 
+{
+    // Instance Variables
+    private Passenger passenger;
+    private Flight flight;
+    private double price;
+    
+    private static int number = 0;
+    
+    // Constructors
+    
+}
